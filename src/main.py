1a111ad0659bfd260b03c14515a0555e790f6e2f
Marcin Ejdys <marcin.ejdys@olx.pl>
2025-06-24 12:31:04 +0200

    mapper

--- src/main.py
+++ src/main.py
@@ -1,7 +1,23 @@
-from textnode import TextNode
+from leafnode import LeafNode
+from textnode import TextNode, TextType
 
 
 print("hello world")
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, props={"href":text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img","",props={"src":text_node.url, "alt": text_node.text})
 
 def main():
     textnode = TextNode("This is some anchor text", "link", "https://www.boot.dev")
