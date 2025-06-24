2e41fe7f75b1ad49feee9a26c58e5e3e32e33287
Marcin Ejdys <marcin.ejdys@olx.pl>
2025-06-24 13:05:26 +0200

    split

--- src/main.py
+++ src/main.py
@@ -19,6 +19,28 @@ def text_node_to_html_node(text_node):
         case TextType.IMAGE:
             return LeafNode("img","",props={"src":text_node.url, "alt": text_node.text})
 
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    temp_text_type = text_type
+    nodes_list = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            nodes_list.append(node)
+        else:
+            splited = node.text.split(delimiter)
+            if len(splited) % 2 == 0:
+                raise Exception(f"missing closing tag")
+            i = 1
+            for split in splited:
+                if i % 2 != 0:
+                    nodes_list.append(TextNode(split,TextType.TEXT))
+                else:
+                    nodes_list.append(TextNode(split, temp_text_type))
+                i += 1
+    
+    return nodes_list
+
+
+
 def main():
     textnode = TextNode("This is some anchor text", "link", "https://www.boot.dev")
     print(textnode)
--- src/test_main.py
+++ src/test_main.py
@@ -1,15 +1,26 @@
 import unittest
 
-from main import text_node_to_html_node
+from main import split_nodes_delimiter, text_node_to_html_node
 from textnode import TextNode, TextType
 
 
 class TestTextNode(unittest.TestCase):
-    def test_text(self):
+    def test_node_to_html(self):
         node = TextNode("This is a text node", TextType.TEXT)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_split_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertRaises("missing closing tag")
+    
+    def test_split_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(len(new_nodes), 3)
+
 
 if __name__ == "__main__":
     unittest.main()
