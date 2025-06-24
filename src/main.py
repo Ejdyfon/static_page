5e070c2039068b96204f5736541719728ad0c9a2
Marcin Ejdys <marcin.ejdys@olx.pl>
2025-06-24 09:36:08 +0200

    added tests

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode
+
+
+print("hello world")
+
+def main():
+    textnode = TextNode("This is some anchor text", "link", "https://www.boot.dev")
+    print(textnode)
+
+main()
