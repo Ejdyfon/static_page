271d995c574991e5983a903aaff39ef61fb1fa65
Marcin Ejdys <marcin.ejdys@olx.pl>
2025-06-26 10:49:16 +0200

    Autogenerating page

--- src/main.py
+++ src/main.py
@@ -14,16 +14,35 @@ def copystatic():
         shutil.rmtree('./public')
     shutil.copytree('./static', './public')
 
+def crawler_content(dir_path):
+    dir = os.listdir(dir_path)
+    for dir in dir:
+        entry_path = os.path.join(dir_path,dir)
+        if os.path.isfile(entry_path):
+            dest = dir_path.replace(dir_path_content, ".")
+            dest_pathhh = os.path.join(dir_path_public,dest)
+            generate_page(
+                entry_path,
+                template_path,
+                os.path.join(dest_pathhh, "index.html"),
+            )
+        else:
+            crawler_content(entry_path)
+
+
+
 def main():
     print("Copying static files to public directory...")
     copystatic()
+    print("Starting crawler...")
+    crawler_content(dir_path_content)
 
-    print("Generating page...")
-    generate_page(
-        os.path.join(dir_path_content, "index.md"),
-        template_path,
-        os.path.join(dir_path_public, "index.html"),
-    )
+    #print("Generating page...")
+    #generate_page(
+    #    os.path.join(dir_path_content, "index.md"),
+    #    template_path,
+    #    os.path.join(dir_path_public, "index.html"),
+    #)
 
 
 main()
