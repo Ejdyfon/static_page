ecbbcf7dc4818be37d2d73c8fb001a30cdbc0bb5
Marcin Ejdys <marcin.ejdys@olx.pl>
2025-06-26 11:14:13 +0200

    page

--- src/main.py
+++ src/main.py
@@ -2,19 +2,20 @@
 
 import os
 import shutil
+import sys
 from gencontent import generate_page
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 def copystatic():
-    if os.path.exists("./public"):
-        shutil.rmtree('./public')
-    shutil.copytree('./static', './public')
+    if os.path.exists("./docs"):
+        shutil.rmtree('./docs')
+    shutil.copytree('./static', './docs')
 
-def crawler_content(dir_path):
+def crawler_content(dir_path, basepath):
     dir = os.listdir(dir_path)
     for dir in dir:
         entry_path = os.path.join(dir_path,dir)
@@ -24,18 +25,26 @@ def crawler_content(dir_path):
             generate_page(
                 entry_path,
                 template_path,
-                os.path.join(dest_pathhh, "index.html"),
+                os.path.join(dest_pathhh, "index.html"), basepath
             )
         else:
-            crawler_content(entry_path)
+            crawler_content(entry_path, basepath)
 
 
 
 def main():
+    if len(sys.argv) > 0:
+        basepath = sys.argv[0]
+    else:
+        basepath = "/"
+
+
+
+
     print("Copying static files to public directory...")
     copystatic()
     print("Starting crawler...")
-    crawler_content(dir_path_content)
+    crawler_content(dir_path_content, basepath)
 
     #print("Generating page...")
     #generate_page(
